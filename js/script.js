3e0f0d6e7939f9b7cb8aefc3c4820a04a973e3e2
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-03-19 23:48:52 +0300

    html-css-finished

---
+++ js/script.js
@@ -0,0 +1,67 @@
+const bookTitle = document.querySelector('[name = "title"]');
+const Author = document.querySelector('[name = "author"]');
+const page = document.querySelector('[name = "page"]');
+const readStatus = document.querySelector('select');
+const addBtn = document.querySelector('.add-btn');
+const form = document.querySelector('form');
+const tableBody = document.querySelector('.table-body')
+
+let myLibrary = JSON.parse(localStorage.getItem('books')) || [];
+
+class Book{
+    constructor(title, author, status){
+        this.title = title;
+        this.author = author;
+        this.read = status;
+    };
+};
+
+function removeItem(id) {
+	const itemToDelete = document.getElementById(`${id}`)
+	myLibrary.splice(itemToDelete.id, 1)
+	handleStorageAndDisplay()
+}
+function handleStorageAndDisplay() {
+	localStorage.setItem('books', JSON.stringify(myLibrary))
+	populateLibrary(myLibrary, itemsList)
+}
+function toggleStatus(e) {
+	if (e.target.classList.contains('read-status')) {
+		const index = e.target.parentNode.id
+		if (myLibrary[index].read === 'read') {
+			myLibrary[index].read = 'not read'
+		} else {
+			myLibrary[index].read = 'read'
+		}
+		handleStorageAndDisplay()
+	}
+}
+function addBookToLibrary(e) {
+	e.preventDefault()
+	if (bookTitle.value === '' || bookAuthor.value === '' || readStatus.value === '') {
+		alert(`Please fill the form.`)
+		return
+	} else {
+		const newItem = new Book(bookTitle.value, bookAuthor.value, readStatus.value)
+		myLibrary.push(newItem)
+		handleStorageAndDisplay()
+		form.reset()
+	}
+}
+function populateLibrary(myLibrary, itemsList) {
+	itemsList.innerHTML = myLibrary
+		.map((book, id) => {
+			return `<tr class="table-item" id="${id}">
+			<td>${book.title}</td>
+			<td>${book.author}</td>
+			<td class="read-status">${book.read}</td>
+			<td><button class="table-item-button" id="${id}" onclick="removeItem(${id})">delete</button>
+			</tr>`
+		})
+		.join('')
+}
+
+populateLibrary(myLibrary, tableBody)
+
+form.addEventListener('submit', addBookToLibrary)
+tableBody.addEventListener('click', toggleStatus)
